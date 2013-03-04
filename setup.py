83b8a850333055536e05f2ab2be26f6e8978af56
Will Hall <will@gnatter.net>
2013-03-04 12:49:35 +0000

    added mac OSX installer

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+import sys
 from setuptools import setup
 from pydosh import version
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+if sys.platform == 'darwin':
+	extra_options = {
+		setup_requires=['py2app'],
+		app=['pydosh/pydosh.py'],
+		options={
+			py2app: {
+				'argv_emulation': True,
+				'includes': ['sip', 'PyQt4']
+			},
+		},
+	}
+elif sys.platform == 'win32':
+	raise Exception('Sorry, Windows is not supported. Please upgrade to Unix')
+else: # Unix
+	extra_options = {
+		scripts=['scripts/pydosh'],
+	}
 
 setup(name='pydosh',
 	version=version.__VERSION__,
@@ -12,11 +31,12 @@ setup(name='pydosh',
 	url='http://github.com/innerhippy/pydosh',
 	author='Will Hall',
 	author_email='will@innerhippy.com',
-	license='LICENSE.txt',
+	license='GPL3',
 	packages=['pydosh'],
     data_files=[
         ('share/applications', ['pydosh.desktop']),
         ('share/pixmaps', ['ui/icons/pydosh.png', 'ui/icons/pydosh.xpm']),
     ],
     scripts = ['scripts/pydosh'],
-	zip_safe=False)
+	zip_safe=False,
+	**extra_options)
