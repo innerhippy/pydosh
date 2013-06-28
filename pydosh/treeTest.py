7d72dd3b38e37777e77b1a1f832f799f39ca6707
Will Hall <will@innerhippy.com>
2013-06-28 22:32:04 +0100

    working test tree

--- pydosh/treeTest.py
+++ pydosh/treeTest.py
@@ -7,42 +7,14 @@ from dialogs import unicode_csv_reader
 
 def main():
 	app = QtGui.QApplication(sys.argv)
-#	root = QtCore.QObject()
-#	root.setObjectName("root")
-#
-#	foo = QtCore.QObject(root) 
-#	foo.setObjectName( "foo" )
-#	child = QtCore.QObject( foo )
-#	child.setObjectName( "Mark" )
-#	child = QtCore.QObject( foo )
-#	child.setObjectName( "Bob" )
-#	child = QtCore.QObject( foo )
-#	child.setObjectName( "Kent" )
-#	
-#	bar = QtCore.QObject(root)
-#	bar.setObjectName( "bar" )
-#
-#	child = QtCore.QObject( bar )
-#	child.setObjectName( "Ole" )
-#	child = QtCore.QObject( bar )
-#	child.setObjectName( "Trond" )
-#	child = QtCore.QObject( bar )
-#	child.setObjectName( "Kjetil" )
-#	child = QtCore.QObject( bar );
-#	child.setObjectName( "Lasse" )
-#
-#	baz = QtCore.QObject(root)
-#	baz.setObjectName( "baz" )
-#	child = QtCore.QObject( baz )
-#	child.setObjectName( "Bengt" )
-#	child = QtCore.QObject( baz )
-#	child.setObjectName( "Sven" )
-
-	root = TreeItem()
-	row1 = root.addChild(TreeItem(['aaa'], root))
-	row2 = root.addChild(TreeItem(['bbb', 'ccc'], root))
+	root = TreeItem()#['one', 'two'])
+	root.appendChild(TreeItem(['aaa']))
+	row1 = TreeItem(['bbb', 'ccc'])
+	root.appendChild(row1)
+	row1.appendChild(TreeItem(['xxx']))
+	row1.appendChild(TreeItem(['yyy', 'zzz']))
 	
-	model = ObjectTreeModel(root)
+	model = TreeModel(root)
 
 	tree = QtGui.QTreeView()
 	tree.setModel(model)
@@ -52,13 +24,17 @@ def main():
 	return app.exec_()
 
 class TreeItem(object):
-	def __init__(self, data=[], parent=None):
+	def __init__(self, data=[]):
 		super(TreeItem, self).__init__()
 		self._data = data
-		self._parent = parent
+		self._parent = None
 		self._children = []
 
-	def addChild(self, child):
+	def setParent(self, parent):
+		self._parent = parent
+
+	def appendChild(self, child):
+		child.setParent(self)
 		self._children.append(child)
 
 	def child(self, row):
@@ -66,73 +42,63 @@ class TreeItem(object):
 
 	def childCount(self):
 		return len(self._children)
-	
+
 	def columnCount(self):
+		if self._parent is None:
+			return self._children[0].columnCount()
 		return len(self._data)
 
 	def data(self, column):
+		if column >= len(self._data):
+			return QtCore.QVariant()
 		return self._data[column]
 
 	def parent(self):
 		return self._parent
 
+	def indexOf(self, child):
+		return self._children.index(child)
+
 	def row(self):
-		if self._parent is not None:
-			return self._parent.index(self)
+		if self._parent:
+			return self._parent.indexOf(self)
 		return 0
 
-class ObjectTreeModel(QtCore.QAbstractItemModel):
-	def __init__(self, root, parent=None):
-		super(ObjectTreeModel, self).__init__(parent=parent)
-		self.root = root
+	def __str__(self):
+		return '%s (%d)' % (', '.join(self._data), id(self))
 
-	def flags(self, index):
-		return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+class TreeModel(QtCore.QAbstractItemModel):
+	def __init__(self, root, parent=None):
+		super(TreeModel, self).__init__(parent=parent)
+		self._root = root
+
+	def columnCount(self, parent=QtCore.QModelIndex()):
+		if parent.isValid():
+			return parent.internalPointer().columnCount()
+		return self._root.columnCount()
 
 	def data(self, index, role=QtCore.Qt.DisplayRole):
 
 		if not index.isValid():
 			return QtCore.QVariant()
 
-		if role == QtCore.Qt.DisplayRole:
-			if index.column() == 0:
-				return index.internalPointer().objectName()
-			elif index.column() == 1:
-				return index.internalPointer().metaObject().className()
-	
-		elif role == QtCore.Qt.ToolTipRole:
-			if index.column() == 0:
-				return 'The name of the object.'
-			if index.column() == 1:
-				return 'The name of the class.'
-
-		return QtCore.QVariant()
-
-	def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-		if role != QtCore.Qt.DisplayRole or orientation != QtCore.Qt.Horizontal:
+		if role != QtCore.Qt.DisplayRole:
 			return QtCore.QVariant()
-		if section == 0:
-			return QtCore.QString( "Object" )
-		elif section == 1:
-			return QtCore.QString( "Class" )
-		return QtCore.QVariant()
+		
+		item = index.internalPointer()
+		return item.data(index.column())
 
-	def rowCount(self, parent=QtCore.QModelIndex()):
-
-		if parent.column() > 0:
+	def flags(self, index):
+		if not index.isValid():
 			return 0
 
-		if not parent.isValid():
-			parent = self.root
-		else:
-			parent = parent.internalPointer()
+		return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
 
-		return parent.childCount()
+	def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+		if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+			return self._root.data(section)
 
-	def columnCount(self, parent=QtCore.QModelIndex()):
-		if parent.isValid():
-			return parent.internalPointer().columnCount()
-		return self.root.columnCount()
+		return QtCore.QVariant()
 
 	def index(self, row, column, parent=QtCore.QModelIndex() ):
 
@@ -140,30 +106,41 @@ class ObjectTreeModel(QtCore.QAbstractItemModel):
 			return QtCore.QModelIndex()
 
 		if not parent.isValid():
-			parentObject = self.root
+			parentItem = self._root
 		else:
-			parentObject = parent.internalPointer()
+			parentItem = parent.internalPointer()
 
-		childItem = parentObject.child(row)
+		childItem = parentItem.child(row)
 
 		if childItem:
-			return self.createIndex( row, column, childItem )
-		else:
-			return QtCore.QModelIndex()
+			return self.createIndex( row, column, childItem)
+
+		return QtCore.QModelIndex()
 
 	def parent(self, index):
 
 		if not index.isValid():
 			return QtCore.QModelIndex()
 
-		child = index.internalPointer()
-		parent = child.parent()
+		childItem = index.internalPointer()
+		parentItem = childItem.parent()
 
-		if parent == self.root:
+		if parentItem == self._root:
 			return QtCore.QModelIndex()
 
-		return self.createIndex(parent.row(), 0, parent)
+		return self.createIndex(parentItem.row(), 0, parentItem)
 
+	def rowCount(self, parent=QtCore.QModelIndex()):
+
+		if parent.column() > 0:
+			return 0
+
+		if not parent.isValid():
+			parentItem = self._root
+		else:
+			parentItem = parent.internalPointer()
+
+		return parentItem.childCount()
 
 class Model(QtCore.QAbstractItemModel):
 	def __init__(self, csvFiles, parent=None):
