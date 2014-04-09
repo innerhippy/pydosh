d6037980d3783211cceaf269dce7c6018789bead
Will Hall <will@gnatter.net>
2014-04-09 11:10:33 +0100

    Done sql migration script

--- sql/migration-2.2.sql
+++ sql/migration-2.2.sql
@@ -18,22 +18,6 @@ CREATE SEQUENCE accounts_id_seq
 
 ALTER SEQUENCE accounts_id_seq OWNED BY accounts.id;
 
--- ??
-ALTER TABLE ONLY accounts
-	ALTER COLUMN id SET DEFAULT nextval('accounts_id_seq'::regclass);
-ALTER TABLE ONLY accounts
-	ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY accounts
-	ADD CONSTRAINT accounts_unique UNIQUE (name, userid);
-ALTER TABLE ONLY accounts
-	ADD CONSTRAINT accounts_userid_fkey FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE CASCADE;
-ALTER TABLE ONLY accounts
-	ADD CONSTRAINT accounts_accounttypeid_fkey FOREIGN KEY (accounttypeid) REFERENCES accounttypes(accounttypeid) ON DELETE CASCADE;
-
-GRANT ALL ON TABLE accounts TO PUBLIC;
-
---- accountshare table
-
 CREATE TABLE accountshare (
 	id integer NOT NULL,
     accountid integer NOT NULL,
@@ -48,35 +32,98 @@ CREATE SEQUENCE accountshare_id_seq
     CACHE 1;
 
 ALTER SEQUENCE accountshare_id_seq OWNED BY accountshare.id;
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_unique UNIQUE (name, userid);
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_accounttypeid_fkey FOREIGN KEY (accounttypeid) REFERENCES accounttypes(accounttypeid) ON DELETE CASCADE;
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_userid_fkey FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE CASCADE;
+ALTER TABLE ONLY accounts ALTER COLUMN id SET DEFAULT nextval('accounts_id_seq'::regclass);
 
-ALTER TABLE ONLY accountshare
-    ADD CONSTRAINT accountshare_pkey PRIMARY KEY (accountid, userid);
-ALTER TABLE ONLY accountshare
-    ADD CONSTRAINT accountshare_accounts_fkey FOREIGN KEY (accountid) REFERENCES accounts(id) ON DELETE CASCADE;
-ALTER TABLE ONLY accountshare
-    ADD CONSTRAINT accountshare_users_fkey FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE CASCADE;
+ALTER TABLE ONLY accountshare ALTER COLUMN id SET DEFAULT nextval('accountshare_id_seq'::regclass);
+ALTER TABLE ONLY accountshare ADD CONSTRAINT accountshare_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY accountshare ADD CONSTRAINT accountshare_unique_share UNIQUE (accountid, userid);
+ALTER TABLE ONLY accountshare ADD CONSTRAINT accountshare_accounts_fkey FOREIGN KEY (accountid) REFERENCES accounts(id) ON DELETE CASCADE;
+ALTER TABLE ONLY accountshare ADD CONSTRAINT accountshare_users_fkey FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE CASCADE;
+CREATE INDEX fki_accountaccountshare_accounts_fkey ON accountshare USING btree (accountid);
+CREATE INDEX fki_accountshare_users_fkey ON accountshare USING btree (userid);
 
-GRANT ALL ON TABLE accountshare TO PUBLIC;
+ALTER TABLE ONLY records ADD COLUMN accountid INTEGER;
+ALTER TABLE ONLY records DROP CONSTRAINT records_unique_records;
 
--- update records table
-
-ALTER TABLE records ADD COLUMN accountid INTEGER;
-
-ALTER TABLE ONLY records
-    ADD CONSTRAINT records_accounts_fkey FOREIGN KEY (accountid) REFERENCES accounts(id);
-
-
-INSERT INTO accounts VALUES (1, 1, 'Nat West', NULL, NULL, 2);
-INSERT INTO accounts VALUES (2, 2, 'Nat West Matercard', NULL, NULL, 2);
-INSERT INTO accounts VALUES (6, 6, 'PayPal', NULL, NULL, 2);
-INSERT INTO accounts VALUES (5, 7, 'First Direct', '50812951', '40 47 60', 2);
-INSERT INTO accounts VALUES (4, 8, 'Santander Shared', '27158440', '09 01 28', 2);
-INSERT INTO accounts VALUES (8, 1, 'Nat West', NULL, NULL, 1);
-INSERT INTO accounts VALUES (10, 2, 'Nat West Mastercard', NULL, NULL, 1);
-INSERT INTO accounts VALUES (3, 4, 'French', '60201325357', NULL, 2);
+-- Populate accounts table
+INSERT INTO accounts (accounttypeid, name, userid) SELECT DISTINCT at.accounttypeid, at.accountname, userid FROM accounttypes at INNER JOIN records r ON r.accounttypeid = at.accounttypeid;
 
 
 UPDATE records r SET accountid=a.id FROM accounts a WHERE a.accounttypeid=r.accounttypeid AND r.userid=a.userid;
 
 ALTER TABLE records DROP accounttypeid;
 ALTER TABLE records DROP userid;
+
+ALTER TABLE ONLY records ADD CONSTRAINT records_unique_records UNIQUE (accountid, checksum);
+ALTER TABLE ONLY records ADD CONSTRAINT records_accounts_fkey FOREIGN KEY (accountid) REFERENCES accounts(id);
+
+REVOKE ALL ON TABLE accounts FROM PUBLIC;
+REVOKE ALL ON TABLE accounts FROM postgres;
+GRANT ALL ON TABLE accounts TO postgres;
+GRANT ALL ON TABLE accounts TO PUBLIC;
+
+REVOKE ALL ON TABLE accountshare FROM PUBLIC;
+REVOKE ALL ON TABLE accountshare FROM postgres;
+GRANT ALL ON TABLE accountshare TO postgres;
+GRANT ALL ON TABLE accountshare TO PUBLIC;
+
+REVOKE ALL ON TABLE records FROM PUBLIC;
+REVOKE ALL ON TABLE records FROM postgres;
+GRANT ALL ON TABLE records TO postgres;
+GRANT ALL ON TABLE records TO PUBLIC;
+
+REVOKE ALL ON TABLE recordtags FROM PUBLIC;
+REVOKE ALL ON TABLE recordtags FROM postgres;
+GRANT ALL ON TABLE recordtags TO postgres;
+GRANT ALL ON TABLE recordtags TO PUBLIC;
+
+REVOKE ALL ON TABLE tags FROM PUBLIC;
+REVOKE ALL ON TABLE tags FROM postgres;
+GRANT ALL ON TABLE tags TO postgres;
+GRANT ALL ON TABLE tags TO PUBLIC;
+
+REVOKE ALL ON TABLE users FROM PUBLIC;
+REVOKE ALL ON TABLE users FROM postgres;
+GRANT ALL ON TABLE users TO postgres;
+GRANT ALL ON TABLE users TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE accounts_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE accounts_id_seq FROM postgres;
+GRANT ALL ON SEQUENCE accounts_id_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE accounts_id_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE accountshare_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE accountshare_id_seq FROM postgres;
+GRANT ALL ON SEQUENCE accountshare_id_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE accountshare_id_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE accounttypes_accountypeid_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE accounttypes_accountypeid_seq FROM postgres;
+GRANT ALL ON SEQUENCE accounttypes_accountypeid_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE accounttypes_accountypeid_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE records_recordid_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE records_recordid_seq FROM postgres;
+GRANT ALL ON SEQUENCE records_recordid_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE records_recordid_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE recordtags_recordtagid_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE recordtags_recordtagid_seq FROM postgres;
+GRANT ALL ON SEQUENCE recordtags_recordtagid_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE recordtags_recordtagid_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE tags_tagid_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE tags_tagid_seq FROM postgres;
+GRANT ALL ON SEQUENCE tags_tagid_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE tags_tagid_seq TO PUBLIC;
+
+REVOKE ALL ON SEQUENCE users_userid_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE users_userid_seq FROM postgres;
+GRANT ALL ON SEQUENCE users_userid_seq TO postgres;
+GRANT SELECT,UPDATE ON SEQUENCE users_userid_seq TO PUBLIC;
+
+
